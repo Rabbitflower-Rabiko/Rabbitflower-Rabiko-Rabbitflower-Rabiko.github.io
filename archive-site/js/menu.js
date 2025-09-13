4919f367c19fa423679a2f691fb04551ca5eafd2
Rabbitflower <143068452+Rabbitflower-Rabiko@users.noreply.github.com>
2025-09-13 23:02:06 +0900

    Update menu.js

--- archive-site/js/menu.js
+++ archive-site/js/menu.js
@@ -1,15 +1,12 @@
-console.log("menu.js が読み込まれました！");
-
 document.addEventListener("DOMContentLoaded", function() {
   const btn = document.querySelector(".menu-toggle");
   const sidebar = document.querySelector(".sidebar");
 
   if (btn && sidebar) {
     btn.addEventListener("click", () => {
-      console.log("ボタンが押されました！");
+      
       sidebar.classList.toggle("open");
     });
   } else {
-    console.log("btn または sidebar が見つかりません");
   }
 });
