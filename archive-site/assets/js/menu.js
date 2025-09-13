b6733337074bfbacac9b88bc4f7b3b1220519b0e
Rabbitflower <143068452+Rabbitflower-Rabiko@users.noreply.github.com>
2025-09-13 17:08:06 +0900

    Update menu.js

--- archive-site/assets/js/menu.js
+++ archive-site/assets/js/menu.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
   const btn = document.querySelector(".menu-toggle");
   const sidebar = document.querySelector(".sidebar");
 
-  btn.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-  });
+  if (btn && sidebar) {
+    btn.addEventListener("click", () => {
+      sidebar.classList.toggle("active");
+    });
+  }
 });
